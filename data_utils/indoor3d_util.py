7c4d3ee0f7084b8e1bb3658065cb55b83a657b1c
Jingtao Li - jingtao1@asu.edu -  - Grad Research Assistant - Graduate Student - Student - Sch Elect Comptr Jingtao1 Energy Engr <lijingtaoz@hotmail.com>
2022-03-21 18:24:24 -0700

    update semantic segmentation training scripts

--- data_utils/indoor3d_util.py
+++ data_utils/indoor3d_util.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
 
-DATA_PATH = os.path.join(ROOT_DIR, 'data','s3dis', 'Stanford3dDataset_v1.2_Aligned_Version')
+DATA_PATH = os.path.join(ROOT_DIR, 'data', 'Stanford3dDataset_v1.2_Aligned_Version')
 g_classes = [x.rstrip() for x in open(os.path.join(BASE_DIR, 'meta/class_names.txt'))]
 g_class2label = {cls: i for i,cls in enumerate(g_classes)}
 g_class2color = {'ceiling':	[0,255,0],
